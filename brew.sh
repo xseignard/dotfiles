bf7d649bd58fee23d3da2f77d404bd83d3d5b3a8
xseignard <xavier.seignard@gmail.com>
2019-10-16 19:40:09 +0200

    Added chrome to brew

--- brew.sh
+++ brew.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-brew cask install iterm2 keybase slack visual-studio-code spotify firefox transmission vlc intel-power-gadget skype brave-browser && brew install gpg
+brew cask install iterm2 keybase slack google-chrome visual-studio-code spotify firefox transmission vlc intel-power-gadget skype brave-browser && brew install gpg
