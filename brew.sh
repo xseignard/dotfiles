04af5da4039a64d988dd753bbd77d21f74201011
xseignard <xavier.seignard@gmail.com>
2021-07-16 11:27:00 +0200

    Update

--- brew.sh
+++ brew.sh
@@ -3,25 +3,27 @@ brew install --cask iterm2 \
 keybase \
 slack \
 discord \
-google-chrome \
 visual-studio-code \
 spotify \
-firefox \
 transmission \
 vlc \
 intel-power-gadget \
 karabiner-elements \
 brave-browser \
+google-chrome \
+firefox \
 gpg-suite \
 adoptopenjdk/openjdk/adoptopenjdk8 \
 android-platform-tools \
 android-studio \
+flipper \
 postman \
 gimp \
-dbeaver-community && \
+dbeaver-community \
+figma && \
 brew install gpg \
 platformio \
 cocoapods \
-hub \
+gh \
 watchman \
 postgresql
